88fa464fe59fedf831abdfd2dce6e90bddad6a82
Christos Mouzouris <mouzouris.christos21@gmail.com>
2025-10-05 14:46:51 +0100

    About section updated

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -12,16 +12,36 @@ function About() {
           <div className="flex justify-center items-start my-10 px-4">
             <div className="w-full max-w-4xl rounded-xl bg-white/[0.025] backdrop-blur-md border border-white/10">
               <Accordion
-                title="This is the first title"
-                content="This is the content for the first title"
+                title="Overview"
+                content="Tiniest is a high performance link management service that transforms long, complex URLs into concise, shareable link.
+                It's designed for reliability, speed and scalability. Providing instant redirects and actionable analytics for every shortened URL."
               />
               <Accordion
-                title="This is the second title"
-                content="This is the content for the second title"
+                title="Performance & Reliability"
+                content="Every shortening operation is concurrency-safe, ensuring consistent results even under high request loads.
+                The backend is optimised for low-latency and fault tolerance, making it capable of handling high traffic with minimal overhead."
               />
               <Accordion
-                title="This is the third title"
-                content="This is the content for the third title"
+                title="Analytics & Insights"
+                content="The platform tracks detailed click metrics, aggregating data efficiently at the database layer using optimised
+                native queries and projections. Users can identify top-performing links, filter bot traffic and gain visibility into
+                real engagement patterns."
+              />
+              <Accordion
+                title="Technology Stack"
+                content="Built with Java + Spring Boot on the backend and React + TypeScript + Tailwind on the frontend.
+                The PostgreSQL database runs in a Docker container for consistent environment setup and easy deployment.
+                Persistent storage ensures data integrity and performance."
+              />
+              <Accordion
+                title="Design Philosophy"
+                content="The interface emphasises speed, simplicity and responsiveness. All components are mobile first, accessible
+                and visually consistent. Designed to demonstrate both frontend craftmanship and backend discipline."
+              />
+              <Accordion
+                title="Future Improvements"
+                content="Planned enhancements include user authentication and caching strategies to reduce query overhead
+                for frequently accessed URLs."
               />
             </div>
           </div>
